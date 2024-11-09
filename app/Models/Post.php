04ae66fc7c49593aca00a0798566b3429e5885cd
elxz <dansmir2002@gmail.com>
2024-11-09 18:58:42 +0300

    create models/migratios for user/post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class User extends Model
+class Post extends Model
 {
     use HasFactory;
     use SoftDeletes;
 
-    protected $table = 'users';
+    protected $table = 'posts';
 
-    public function posts() {
-        return $this->hasMany(Post::class);
+    public function user() {
+        return $this->belongsTo(User::class);
     }
 }
